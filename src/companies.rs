c2a5f34ae9a857fd6a3b6378b28993a6f4564f09
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2018-07-16 15:03:22 -0400

    formating

--- src/companies.rs
+++ src/companies.rs
@@ -1,62 +1,72 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
 use failure::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 
 #[derive(Debug)]
 pub struct Company {
-	pub name: String,
-	pub first_name: String,
-	pub last_name: String,
-	pub city: String,
-	pub province: String,
-	pub postal_code: String
+    pub name: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub city: String,
+    pub province: String,
+    pub postal_code: String,
 }
 
 impl Company {
-	pub fn from_text(text: &str) -> Result<Self, String> {
-		let mut parts = text.split(" ");
+    pub fn from_text(text: &str) -> Result<Self, String> {
+        let mut parts = text.split(" ");
 
-		let name = parts.next().ok_or(String::from("Empty line?"))?.to_string();
-		let first_name = parts.next().ok_or(String::from("No first name"))?.to_string();
-		let last_name = parts.next().ok_or(String::from("No last name"))?.to_string();
+        let name = parts.next().ok_or(String::from("Empty line?"))?.to_string();
+        let first_name = parts
+            .next()
+            .ok_or(String::from("No first name"))?
+            .to_string();
+        let last_name = parts
+            .next()
+            .ok_or(String::from("No last name"))?
+            .to_string();
 
-		let city = parts.next().ok_or(String::from("No city"))?.to_string();
-		let province = parts.next().ok_or(String::from("No province"))?.to_string();
-		let postal_code = parts.next().ok_or(String::from("No postal code"))?.to_string();
+        let city = parts.next().ok_or(String::from("No city"))?.to_string();
+        let province = parts.next().ok_or(String::from("No province"))?.to_string();
+        let postal_code = parts
+            .next()
+            .ok_or(String::from("No postal code"))?
+            .to_string();
 
-		Ok(Self {
-			name,
-			first_name,
-			last_name,
-			city,
-			province,
-			postal_code
-		})
-	}
+        Ok(Self {
+            name,
+            first_name,
+            last_name,
+            city,
+            province,
+            postal_code,
+        })
+    }
 }
 
 pub fn read_companies() -> Result<Vec<Company>, Error> {
-	let companies_file = File::open("companies.txt")?;
-	let company_reader = BufReader::new(&companies_file);
+    let companies_file = File::open("companies.txt")?;
+    let company_reader = BufReader::new(&companies_file);
 
-	let companies = company_reader.lines().filter_map(|line| {
-		if let Ok(line) = line {
-			let company = Company::from_text(&line);
+    let companies = company_reader
+        .lines()
+        .filter_map(|line| {
+            if let Ok(line) = line {
+                let company = Company::from_text(&line);
 
-			match company {
-				Ok(company) => {
-					Some(company)
-				},
-				Err(error) => {
-					println!("{:?}", error);
-					None
-				}
-			}
-		} else {
-			None
-		}
-	}).collect::<Vec<Company>>();
+                match company {
+                    Ok(company) => Some(company),
+                    Err(error) => {
+                        println!("{:?}", error);
+                        None
+                    }
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<Company>>();
 
-	Ok(companies)
+    Ok(companies)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-extern crate rand;
 extern crate failure;
+extern crate rand;
 
 use failure::Error;
 
+use rand::distributions::range::{Range, SampleRange};
 use rand::prelude::*;
 use rand::thread_rng;
-use rand::distributions::range::{Range, SampleRange};
-use std::fs::File;
-use std::io::Write;
-use std::io::BufWriter;
 use std::char;
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
 
 mod companies;
 mod employees;
 
-const EMPLOYEE_TYPE: [&str; 6] = ["Development", "QA", "UI", "Design", "BusinessIntelligence", "Networking"];
+const EMPLOYEE_TYPE: [&str; 6] = [
+    "Development",
+    "QA",
+    "UI",
+    "Design",
+    "BusinessIntelligence",
+    "Networking",
+];
 
 const SERVICE_TYPE: [&str; 2] = ["Cloud", "On-premises"];
 
 const PREMIUM_TYPE: [&str; 4] = ["Premium", "Gold", "Diamond", "Silver"];
 
 fn main() -> Result<(), Error> {
-	let mut rng = thread_rng();
+    let mut rng = thread_rng();
 
-	let mut out = File::create("ini_data.sql")?;
+    let mut out = File::create("ini_data.sql")?;
 
-	writeln!(out, "DROP TABLE Employees;")?;
-	writeln!(out, "DROP TABLE Contracts;")?;
-	writeln!(out, "DROP TABLE Companies;")?;
+    writeln!(out, "DROP TABLE Employees;")?;
+    writeln!(out, "DROP TABLE Contracts;")?;
+    writeln!(out, "DROP TABLE Companies;")?;
 
-	writeln!(out, "CREATE TABLE Companies(
+    writeln!(
+        out,
+        "CREATE TABLE Companies(
          companyId int NOT NULL AUTO_INCREMENT,
          companyName varchar(255),
          repFirstName varchar(255),
@@ -40,9 +49,12 @@ fn main() -> Result<(), Error> {
          province varchar(255),
          postalCode varchar(255),
          PRIMARY KEY (companyId)
-);")?;
+);"
+    )?;
 
-	writeln!(out, "CREATE TABLE Contracts(
+    writeln!(
+        out,
+        "CREATE TABLE Contracts(
         contractId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         companyId int NOT NULL,
         FOREIGN KEY(companyId) REFERENCES Companies(companyId),
@@ -52,9 +64,10 @@ fn main() -> Result<(), Error> {
         serviceStartDate DATE NOT NULL,
         serviceType ENUM('Cloud','On-premises') NOT NULL,
         premiumType ENUM('Premium','Gold','Diamond','Silver') NOT NULL
-);")?;
+);"
+    )?;
 
-	writeln!(out, "CREATE TABLE Employees (
+    writeln!(out, "CREATE TABLE Employees (
         employeeId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         is_manager BOOL NOT NULL,
         department ENUM('Development', 'QA', 'UI', 'Design', 'BusinessIntelligence', 'Networking')  NOT NULL,
@@ -64,21 +77,23 @@ fn main() -> Result<(), Error> {
         FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
 );")?;
 
-	let companies = companies::read_companies()?;
+    let companies = companies::read_companies()?;
 
-	let mut employees = employees::read_employees()?;
+    let mut employees = employees::read_employees()?;
 
-	let mut last_contract_id = 1;
-	let mut last_company_id = 1;
+    let mut last_contract_id = 1;
+    let mut last_company_id = 1;
 
-	let contract_rng = Range::new(1, 6);
-	let employee_rng = Range::new(5, 12);
+    let contract_rng = Range::new(1, 6);
+    let employee_rng = Range::new(5, 12);
 
-	for (index, company) in companies.into_iter().enumerate() {
+    for (index, company) in companies.into_iter().enumerate() {
+        let email = format!(
+            "{}_{}@{}.com",
+            company.first_name, company.last_name, company.name
+        );
 
-		let email = format!("{}_{}@{}.com", company.first_name, company.last_name, company.name);
-
-		writeln!(out, "INSERT INTO Companies VALUES({id}, '{name}', '{first_name}', NULL, '{last_name}', '{email}', '{city}', '{province}', '{postal_code}');",
+        writeln!(out, "INSERT INTO Companies VALUES({id}, '{name}', '{first_name}', NULL, '{last_name}', '{email}', '{city}', '{province}', '{postal_code}');",
 			id = last_company_id,
 			name = company.name,
 			first_name = company.first_name,
@@ -89,33 +104,36 @@ fn main() -> Result<(), Error> {
 			postal_code = company.postal_code
 		)?;
 
-		let is_nike = company.name == "Nike";
+        let is_nike = company.name == "Nike";
 
+        let number_of_contracts = if is_nike {
+            1
+        } else {
+            contract_rng.sample(&mut rng)
+        };
 
-		let number_of_contracts = if is_nike{1} else {contract_rng.sample(&mut rng)};
+        for _ in 0..number_of_contracts {
+            let contract_type = SERVICE_TYPE[rng.gen_range(0, SERVICE_TYPE.len())];
+            let contract_level = if is_nike {
+                "Gold"
+            } else {
+                PREMIUM_TYPE[rng.gen_range(0, PREMIUM_TYPE.len())]
+            };
 
-		for _ in 0..number_of_contracts {
-			let contract_type = SERVICE_TYPE[rng.gen_range(0, SERVICE_TYPE.len())];
-			let contract_level = if is_nike {
-				"Gold"
-			} else {
-				PREMIUM_TYPE[rng.gen_range(0, PREMIUM_TYPE.len())]
-			};
+            let year = rng.gen_range::<usize>(2016, 2019);
+            let month = rng.gen_range::<usize>(1, 13);
 
-			let year = rng.gen_range::<usize>(2016, 2019);
-			let month = rng.gen_range::<usize>(1, 13);
+            let day = rng.gen_range::<usize>(0, 28);
 
-			let day = rng.gen_range::<usize>(0, 28);
+            let date = format!("{:04}-{:02}-{:02}", year, month, day);
 
-			let date = format!("{:04}-{:02}-{:02}", year, month, day);
+            let mut contact_num = String::new();
 
-			let mut contact_num = String::new();
+            for _ in 0..10 {
+                contact_num.push(char::from_digit(rng.gen_range(0, 10), 10).unwrap());
+            }
 
-			for _ in 0..10 {
-				contact_num.push(char::from_digit(rng.gen_range(0, 10), 10).unwrap());
-			}
-
-			writeln!(out, "INSERT INTO Contracts VALUES({id}, {company_id} ,'{contact_number}', {annual}.00, {initial}.00, '{date}', '{c_type}', '{c_level}');",
+            writeln!(out, "INSERT INTO Contracts VALUES({id}, {company_id} ,'{contact_number}', {annual}.00, {initial}.00, '{date}', '{c_type}', '{c_level}');",
 				id = last_contract_id,
 				company_id = last_company_id,
 				contact_number = contact_num,
@@ -126,27 +144,25 @@ fn main() -> Result<(), Error> {
 				c_level = contract_level
 			)?;
 
+            let number_of_employees = employee_rng.sample(&mut rng);
 
-			let number_of_employees = employee_rng.sample(&mut rng);
+            for i in 0..number_of_employees {
+                let employee = employees.pop().unwrap();
 
-			for i in 0..number_of_employees {
-				let employee = employees.pop().unwrap();
-
-				writeln!(out, "INSERT INTO Employees VALUES(0, {is_manager}, '{dep}', '{first_name}', '{last_name}', {contract_id});",
+                writeln!(out, "INSERT INTO Employees VALUES(0, {is_manager}, '{dep}', '{first_name}', '{last_name}', {contract_id});",
 					dep = EMPLOYEE_TYPE[rng.gen_range(0, EMPLOYEE_TYPE.len())],
 					is_manager = (i == 0) as i32,
 					first_name = employee.first_name,
 					last_name = employee.last_name,
 					contract_id = last_contract_id
 				)?;
-			}
+            }
 
-			last_contract_id += 1;
-		}
+            last_contract_id += 1;
+        }
 
-		last_company_id += 1;
-	}
+        last_company_id += 1;
+    }
 
-	Ok(())
+    Ok(())
 }
-
